6ee75aa6e080983c5da5a4b44ca060467356737b
smallwood13 <smallwood13@gmail.com>
2022-09-26 12:05:35 +0900

    Added version controller.

---
+++ src/SPR.TestDataStorage.WebService/Controllers/VersionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Reflection;
+
+namespace SPR.TestDataStorage.WebService.Controllers
+{
+    [Route("version")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult Get()
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+            return new JsonResult(new VersionModel { Version = version?.ToString() });
+        }
+
+        public class VersionModel
+        {
+            [JsonProperty("version")]
+            public string? Version { get; set; }
+        }
+    }
+}
